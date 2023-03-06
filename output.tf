afa89229061112b82866b7252ce44e81eab93541
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2023-03-06 17:58:07 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,18 +2,3 @@ output "public_ip" {
   description = "AWS Instance Public IP"
   value       = aws_instance.sql_server_instance.public_ip
 }
-/*
-output "rds_endpoint" {
-  description = "RDS Enpoint"
-  value       = aws_db_instance.rds-sql-instance.address
-}
-output "rds_username" {
-  description = "RDS Username"
-  value       = local.db_creds.username
-  sensitive   = true
-}
-output "rds_password" {
-  description = "RDS password"
-  value       = local.db_creds.password
-  sensitive   = true
-}*/
